fe79122697cc389ba75eafd67f95743cc9f95f43
Sagi Saada <33149808+sagikandleker@users.noreply.github.com>
2018-04-10 17:28:11 +0300

    Update Member.h

--- Member.h
+++ Member.h
@@ -1,59 +1,62 @@
-#include <vector>
-#include <set>
-#include <iostream>
-
-using namespace std;
-
-class user{
-
-public:
-	int id;
-	set <int> followers;
-	set <int> following;
-	user();
-
-};
-
-user::user():id(0){}
 class Member{
+
 public:
-	user person;
+static vector<Member*> allUsers;
+set<Member*> following;
+set<Member*> followers;
 
-	static int nummembers;
-	static vector<user*> alluser;
+Member();
+~Member();
 
-	Member();
-	~Member();
-	int numFollowers(){
-		return person.followers.size();
-	}
-	int numFollowing(){
-		return person.following.size();
-	}
-	void follow(Member& n){
-		person.following.insert(n.person.id);
-		n.person.followers.insert(person.id);
-	}
-	void unfollow(Member& n){
-		person.following.erase(n.person.id);
-		n.person.followers.erase(person.id);
-	}
-	static int count(){return nummembers;}
-};
-vector <user*> Member::alluser={};
-int Member::nummembers=0;
-Member::Member(){
-
-	nummembers++;
-	person.id=nummembers;
-    alluser.push_back(&person);
+int numFollowers(){
+    return this->followers.size();
 }
+
+int numFollowing(){
+    return this->following.size();
+}
+
+void follow(Member &m){
+    if(this != &m){
+        this->following.insert(&m);
+        m.followers.insert(this);
+    }
+}
+
+void unfollow(Member& m){
+    if(this != &m){
+        this->following.erase(&m);
+        m.followers.erase(this);
+    }
+}
+
+static int count(){
+    return allUsers.size();
+}
+
+};
+
+vector<Member*> Member::allUsers = {};
+
+Member::Member(){
+    allUsers.push_back(this);
+}
+
+
 Member::~Member(){
 
-	nummembers--;
-	for(unsigned int i=0;i<alluser.size();i++){
-		alluser.at(i)->followers.erase(person.id);
-		alluser.at(i)->following.erase(person.id);
-	}
-	alluser.pop_back();
+for(unsigned int i = 0; i < allUsers.size(); i++){
+    allUsers.at(i)->followers.erase(this);
+    allUsers.at(i)->following.erase(this);
+    }
+
+int del = 0;
+for(unsigned int j = 0; j < allUsers.size(); j++){
+    if(this == allUsers.at(j)){
+            del = j;
+        }
+    }
+
+    allUsers.erase(allUsers.begin() + del);
+
 }
